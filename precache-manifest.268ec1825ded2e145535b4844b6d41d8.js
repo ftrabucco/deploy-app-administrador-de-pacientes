b246413c451f125d183d59d4b9ffbef8688035b1
Francisco Trabucco <ftrabucco@vates.org>
2020-10-05 23:42:27 -0300

    Updates

---
+++ precache-manifest.268ec1825ded2e145535b4844b6d41d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853077e4847869b278dd038bc3d4ed73",
+    "url": "/ftrabucco/deploy-app-administrador-de-pacientes/index.html"
+  },
+  {
+    "revision": "1e1f7b17bb5e210c008e",
+    "url": "/ftrabucco/deploy-app-administrador-de-pacientes/static/css/main.13d2ccb7.chunk.css"
+  },
+  {
+    "revision": "26e23c862190e577ecce",
+    "url": "/ftrabucco/deploy-app-administrador-de-pacientes/static/js/2.820bfa35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ftrabucco/deploy-app-administrador-de-pacientes/static/js/2.820bfa35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1f7b17bb5e210c008e",
+    "url": "/ftrabucco/deploy-app-administrador-de-pacientes/static/js/main.16c5c4e6.chunk.js"
+  },
+  {
+    "revision": "67a8fc51d7c9a7ac051e",
+    "url": "/ftrabucco/deploy-app-administrador-de-pacientes/static/js/runtime-main.9f56aa62.js"
+  }
+]);
